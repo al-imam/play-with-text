4259d492f78005c84dc0a9a28d077f6b4d9bf286
al-imam <alimam01828@gmail.com>
2023-09-20 19:07:46 +0600

    step selection formatting

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "play-with-text.kebab-case-v2",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const selection = editor.selection;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "play-with-text" is now active!');
+        const selectedText = editor.document.getText(selection);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('play-with-text.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from play-with-text!');
-	});
+        editor.edit((editBuilder) => {
+          editBuilder.replace(selection, toKabobCase(selectedText));
+        });
+      }
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+function toKabobCase(input: string): string {
+  return input.toLowerCase();
+}
+
 export function deactivate() {}
